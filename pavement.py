2b6e7e290cba1ddf963d866598c8e17ac11fc003
Juergen Bocklage-Ryannel <juergen.bocklage-ryannel@pelagicore.com>
2016-03-19 08:07:44 -0700

    Updated the live reload targets for paver. Ideally I would like to switch to click.py.

--- pavement.py
+++ pavement.py
@@ -1,24 +1,29 @@
 from paver.easy import *
 from livereload import Server, shell
 
+
 @task
 @needs('assets_init', 'build_html', 'build_pdf', 'build_epub', 'build_qt', 'build_assets')
 def build_all():
     pass
 
+
 @task
 def build_html():
     sh('make html')
+
 
 @task
 def build_pdf():
     sh('make latexpdf')
     path('_build/latex/qt5_cadaques.pdf').copy('assets')
 
+
 @task
 def build_epub():
     sh('make epub')
     path('_build/epub/qt5_cadaques.epub').copy('assets')
+
 
 @task
 def build_qt():
@@ -26,14 +31,17 @@ def build_qt():
     sh('qcollectiongenerator _build/qthelp/Qt5CadaquesBook.qhcp')
     path('_build/qthelp/Qt5CadaquesBook.qch').copy('assets')
 
+
 @task
 def show_qt():
     sh('assistant -collectionFile _build/qthelp/Qt5CadaquesBook.qch')
+
 
 @task
 def clean():
     sh('make clean')
     path('assets').rmtree()
+
 
 @task
 def serve():
@@ -44,10 +52,9 @@ def serve():
 @task
 def live():
     server = Server()
-    server.watch('*.py', shell('make html', cwd='.'))
-    server.watch('*.rst', shell('make html', cwd='.'))
-    server.watch('ch*/', shell('make html', cwd='.'))
-    server.serve(root='_build/html', liveport=35729, open_url_delay=1, debug=False)
+    server.watch('en', shell('paver build_html', cwd='.'))
+    server.serve(root='_build/html')
+
 
 ROOT = path('.').abspath()
 ASSETS = path('assets').abspath()
@@ -62,9 +69,10 @@ def assets_init():
 @needs('assets_init')
 def build_assets():
     for ch in path('.').dirs('ch??'):
-        name = '%s-assets.tgz'%ch
+        name = '%s-assets.tgz' % ch
         if ch.joinpath('src').isdir():
             sh('tar czvf assets/{0} --exclude=".*" {1}/src/'.format(name, ch))
+
 
 @task
 @needs('build_all')
@@ -77,3 +85,4 @@ def publish():
         sh('git add .')
         sh('git commit -m "update"')
         sh('git push')
+
